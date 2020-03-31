2fa61e2fa065894f53c7d2e6c9ade72595cf8508
lzw-pku <lizhenwen@pku.edu.cn>
2020-03-31 17:52:21 +0800

    add sql2nl

--- script.py
+++ script.py
@@ -1,15 +1,28 @@
 import json
-for i in range(7000):
-    with open(f'./data/sql/{i}.sql') as f:
-        x = f.readlines()[0]
-    with open(f'./data/sqlout/{i}.sql') as f:
-        y = f.readlines()[0]
-    '''
-    if x != y:
-        print(i)
-        print(x)
-        print(y)
+with open('data/train_spider.json') as f:
+    data = json.load(f)
+with open('data/tables.json') as f:
+    table = json.load(f)
+with open('data/train.json') as f:
+    semql = json.load(f)
+for t in table:
+    if t['db_id'] == 'college_1':
+        print(t)
         break
-    '''
-    if len(x.split()) > 80:
-        print(x)
+print(semql[0].keys())
+#exit(0)
+for s in semql:
+    if s['question'] == 'Find the first name of student who is taking classes from accounting and Computer Info. Systems departments':
+        print(s)
+        print(s['rule_label'])
+        print(s['query'])
+        exit(0)
+        break
+print(data[0].keys())
+print(data[0]['query'])
+print(data[0]['db_id'])
+for d in data:
+    if len(d['query'].split()) > 80:
+        print(d['question'])
+        print(d['query'])
+        print(d['db_id'])
